2b64138d16ee2fee48a53a8e76c9a7b5292433a4
simon.provencherlepa <simon.provencherlepa@rci.rogers.com>
2018-04-10 09:20:11 -0400

    adding new models

--- src/app/components/articles/articles.component.ts
+++ src/app/components/articles/articles.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-articles',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticlesComponent implements OnInit {
 
-  constructor() { }
+  articles: any;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('/articles').subscribe(data => {
+      this.articles = data;
+    });
   }
 
 }
